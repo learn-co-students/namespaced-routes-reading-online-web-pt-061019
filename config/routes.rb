dff2318add1eb8cc7e58a03c7d239414a2d50977
Khris Punzalan <khrispunzalan@gmail.com>
2019-11-15 05:39:05 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  # get '/admin/stats', to: 'stats#index'
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
 
   root 'posts#index'
 end
+
+# When we want to route with a module and use that module's name as the URL prefix, we can use the namespace method instead of scope, module
+# helper is now prefixed with admin_, so stats_path becomes admin_stats_path

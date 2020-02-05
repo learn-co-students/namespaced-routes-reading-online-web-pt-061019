091933478027c144ae524a347a67cfe13539040a
Amber Taranto <ambzzz@gmail.com>
2020-02-04 18:12:34 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
+  namespace :admin do 
+    resources :stats, only: [:index]
+  end
+
   get '/admin/stats', to: 'stats#index'
 
   root 'posts#index'
